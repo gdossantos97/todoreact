d155274320978924feff81824f96e66fc69752dc
Gabriel <gabe.cs1997@gmail.com>
2022-02-26 00:02:37 -0500

    Second Commit - changed index.js

--- index.js
+++ index.js
@@ -14,11 +14,29 @@ function App(){
     }        
   ])
 
+const [value, setValue] = React.useState('');
+const handleSubmit = e => {
+  e.preventDefault();
+  if (!value) return;
+  const newTodos = [...todos, {text:value, isCompleted:false}];
+  setTodos(newTodos);
+  setValue('');
+}
+
+
   return(
     <>
-      {todos.map((todo, i) => (
-        <div className="todo" key={i}>{todo.text}</div>
-      ))}
+      {todos.map((todo, i) => 
+          <div className="todo" key={i}>{todo.text}</div> )}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="input"
+            value={value}
+            placeholder="Add Todo ..."
+            onChange={e => setValue(e.target.value)}
+          />
+        </form>
     </>
   );
 }
